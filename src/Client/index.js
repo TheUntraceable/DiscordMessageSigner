cc67cd4a0c1ae8a2a68e50cb1ad7d17cd9bfb187
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-18 17:17:48 +0100

    Switch back to JS

--- src/Client/index.js
+++ src/Client/index.js
@@ -2,15 +2,18 @@ import { Client } from "@theuntraceable/discord-rpc";
 import configFile from "../config.json" assert { type: "json" };
 import tokenFile from "./token.json" assert { type: "json" };
 import fs from "fs";
+
 const config = configFile;
-// @ts-ignore
+
 const token = tokenFile;
+
 const publicFile = (fs.readFileSync("../.public")).toString();
 const privateFile = (fs.readFileSync("../.private")).toString();
-// @ts-ignore
+
 const client = new Client({
     transport: "ipc"
 });
+
 client.on("ready", async (message) => {
     console.log(message);
     if (message.access_token) {
@@ -20,14 +23,16 @@ client.on("ready", async (message) => {
         }));
     }
 });
+
 const loginOptions = {
     clientId: config.client_id,
     clientSecret: config.client_secret,
     scopes: ["identify", "rpc", "messages.read"],
     redirectUri: "https://discord.com"
 };
-// @ts-ignore
+
 if (token.token && new Date(token.expiresAt) > Date()) {
     loginOptions.accessToken = token.token;
 }
+
 client.login(loginOptions);
