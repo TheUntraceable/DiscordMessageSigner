0873b0b543642b4eeae2b5f3de69bcb928675a18
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-18 13:17:54 +0100

    Ignore a few more files

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,5 +1,33 @@
 import { Client } from "@theuntraceable/discord-rpc"
 import { NodeRSA } from "node-rsa"
-import config from 
+import config from "../config.json" assert {type: "json"}
+import token from "./token.json" assert {type: "json"}
+import fs from "fs/promises"
 
-const client = new Client({})
+const client: Client = new Client({
+    transport: "ipc"
+})
+
+interface ReadyPayload {
+    accessToken?: String
+    expiresAt?: String
+    user?: {
+        id: String
+        username: String
+        discriminator: String
+        avatar: String
+    },
+    scopes: String[]
+}
+
+client.on("ready", async (message: ReadyPayload) => {
+    if(message.accessToken) {
+        await fs.writeFile("./token.json", JSON.stringify({
+            token: message.accessToken,
+            expiresAt: message.expiresAt
+        }))
+    }
+
+
+})
+
